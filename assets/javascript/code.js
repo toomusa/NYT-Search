712a6488b88ae25f510f9849a8e5536cb0a52691
cLifFORD JoRGenSen <cliffordjorgensen@gmail.com>
2019-06-04 14:42:05 -0700

    created click events

--- assets/javascript/code.js
+++ assets/javascript/code.js
@@ -1,11 +1,36 @@
+// variable to hold start year
+var startYear;
+//variable end year
+var endYear;
+// variable holds user search term
 var searchTerm;
+//variable to hold specified number of records
+var numRecords;
+//search button variable holds html searchButton ID
 var searchButton = $("#searchButton");
+//clear results variable holds html clearResults ID
 var clearResults = $("#clearResults");
 
-var yearRange = function (start, end) { };
+searchButton.on("click", function () {
+    search = $("#searchButton").val();
+    start = $("#start").val();
+    end = $("#end").val();
+}
+
+    // ajax call
+    // API Key GevTC67WTlBRpmBttQ4SDfrAxT6N0VRH
+
+    let queryURL = `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${query1}&api-key=GevTC67WTlBRpmBttQ4SDfrAxT6N0VRH`;
 
 
-// ajax call
-// API Key GevTC67WTlBRpmBttQ4SDfrAxT6N0VRH
 
-let queryURL = `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${query1}&api-key=GevTC67WTlBRpmBttQ4SDfrAxT6N0VRH`;
+
+
+
+
+
+clearResults.on("click", function () {
+    $("#searchButton").val('');
+    $("start").val('');
+    $("end").val('');
+});
